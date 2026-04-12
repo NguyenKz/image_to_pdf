58e8adf9484726bfaa3151065343b5d7bdec2a80
nguyenkz <nguyen.tran@stacktech.org>
2026-04-12 17:19:58 +0700

    Update version to 1.0.1 and add update/download URLs in dowload_zalo_album.js

--- dowload_zalo_album.js
+++ dowload_zalo_album.js
@@ -1,10 +1,12 @@
 // ==UserScript==
 // @name         Zalo Chat XPath Image Downloader
 // @namespace    https://chat.zalo.me/
-// @version      1.0.0
+// @version      1.0.1
 // @description  Nhap XPath, dem anh va tai anh theo thu tu tren Zalo Chat
 // @author       GPT
 // @match        https://chat.zalo.me/*
+// @updateURL    https://raw.githubusercontent.com/NguyenKz/image_to_pdf/main/dowload_zalo_album.js
+// @downloadURL  https://raw.githubusercontent.com/NguyenKz/image_to_pdf/main/dowload_zalo_album.js
 // @run-at       document-idle
 // @grant        none
 // ==/UserScript==
