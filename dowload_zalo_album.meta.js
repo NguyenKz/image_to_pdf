54389a30391a964bd9ed9208bfbfb9cc9d351fa1
nguyenkz <nguyen.tran@stacktech.org>
2026-04-12 17:23:22 +0700

    Add Zalo Chat XPath Image Downloader script with initial implementation in dowload_zalo_album.user.js and metadata in dowload_zalo_album.meta.js

---
+++ dowload_zalo_album.meta.js
@@ -0,0 +1,12 @@
+// ==UserScript==
+// @name         Zalo Chat XPath Image Downloader
+// @namespace    https://chat.zalo.me/
+// @version      1.0.1
+// @description  Nhap XPath, dem anh va tai anh theo thu tu tren Zalo Chat
+// @author       GPT
+// @match        https://chat.zalo.me/*
+// @updateURL    https://raw.githubusercontent.com/NguyenKz/image_to_pdf/main/dowload_zalo_album.meta.js
+// @downloadURL  https://raw.githubusercontent.com/NguyenKz/image_to_pdf/main/dowload_zalo_album.user.js
+// @run-at       document-idle
+// @grant        none
+// ==/UserScript==
--- dowload_zalo_album.user.js
+++ dowload_zalo_album.user.js
@@ -5,8 +5,8 @@
 // @description  Nhap XPath, dem anh va tai anh theo thu tu tren Zalo Chat
 // @author       GPT
 // @match        https://chat.zalo.me/*
-// @updateURL    https://raw.githubusercontent.com/NguyenKz/image_to_pdf/main/dowload_zalo_album.js
-// @downloadURL  https://raw.githubusercontent.com/NguyenKz/image_to_pdf/main/dowload_zalo_album.js
+// @updateURL    https://raw.githubusercontent.com/NguyenKz/image_to_pdf/main/dowload_zalo_album.meta.js
+// @downloadURL  https://raw.githubusercontent.com/NguyenKz/image_to_pdf/main/dowload_zalo_album.user.js
 // @run-at       document-idle
 // @grant        none
 // ==/UserScript==
